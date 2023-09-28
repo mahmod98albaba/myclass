4143841195a69b08ef8aca98136a3bec003b1998
Mahmoud Al-baba <131158371+mahmod98albaba@users.noreply.github.com>
2023-09-28 06:16:36 +0300

    just replace puplic to public

--- myclass.java
+++ myclass.java
@@ -11,40 +11,38 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-puplic class myclass {
+public class myclass {
 
-	WebDriver driver = new ChromeDriver();
-	SoftAssert myAssertion = new SoftAssert();
-	String expectedCurrency = "SAR"; 
-	String expectedLang = "العربية";
+    WebDriver driver = new ChromeDriver();
+    SoftAssert myAssertion = new SoftAssert();
+    String expectedCurrency = "SAR";
+    String expectedLang = "العربية";
 
-	@BeforeTest
-	public void setup() {
-	}
+    @BeforeTest
+    public void setup() {
+    }
 
-	@Test
-	public void testAlmosaferWebsite() {
-		driver.get("https://www.almosafer.com/en");
-		driver.findElement(By.className("cta__button")).click();
+    @Test
+    public void testAlmosaferWebsite() {
+        driver.get("https://www.almosafer.com/en");
+        driver.findElement(By.className("cta__button")).click();
 
-		String ActualCurrency = driver.findElement(By.className("fPnvOO")).getText();
-		myAssertion.assertEquals(ActualCurrency, expectedCurrency, "Currency  match");
+        String ActualCurrency = driver.findElement(By.className("fPnvOO")).getText();
+        myAssertion.assertEquals(ActualCurrency, expectedCurrency, "Currency  match");
 
-		
-		String ActualLang = driver.findElement(By.className("jJNggu")).getText();
-		myAssertion.assertEquals(ActualLang, expectedLang, "Language  match");
-		
-		
+        String ActualLang = driver.findElement(By.className("jJNggu")).getText();
+        myAssertion.assertEquals(ActualLang, expectedLang, "Language  match");
+
         WebElement flightsTab = driver.findElement(By.id("uncontrolled-tab-example-tab-flights"));
         boolean isFlightsTabSelected = flightsTab.getAttribute("aria-selected").equals("true");
 
         myAssertion.assertTrue(isFlightsTabSelected, "Flights tab is selected by default");
 
+        myAssertion.assertAll();
+    }
 
-		myAssertion.assertAll();
-	}
-
-	@AfterTest
-	public void teardown() {
-	}
+    @AfterTest
+    public void teardown() {
+        driver.quit();
+    }
 }
